c675c2be6ee017aa3bb2a2684db968f480987588
FlorentA <florent_arnould@yahoo.fr>
2017-02-13 14:28:28 +0100

    ajout id en paramètre GET + contrôle de sécurité

--- analyse.php
+++ analyse.php
@@ -3,8 +3,13 @@
 //paramètres
 $exclure=('porc','E604','ascorbate');
 $url='https://fr.openfoodfacts.org/produit/3166350006271/tagliatelles-a-la-carbonara-marie';
+if (!isset($_GET['id_produit']))
+	die('pas d\'identifiant produit');
+$produit=$_GET['id_produit'];
+if (!ereg('^[0-9]+$',$id_produit)
+	die('Erreur de format de l\'identifiant');
 //analyse
-$page=file($url);
+$page=file($url$id_produit);
 $liste=array_search($exclure,$page);
 //affichage
 echo 'ingrédients exclus';
