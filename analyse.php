af93a73b56a7c5b0f3996b4734320ee7d5e49e7b
FlorentA <florent_arnould@yahoo.fr>
2017-02-13 14:57:59 +0100

    Utiliser Curl à la place de open_basedir

--- analyse.php
+++ analyse.php
@@ -8,7 +8,20 @@ $produit=$_GET['id_produit'];
 if (!ereg('^[0-9]+$',$id_produit)
 	die('Erreur de format de l\'identifiant');
 //analyse
-$page=file($url$id_produit);
+
+//en utilisant curl plutôt que fopen et open_basedir
+$ch = curl_init($url$ip_produit);
+$fp = fopen('produit.html', "w");
+
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+curl_exec($ch);
+curl_close($ch);
+fclose($fp);
+
+//$page=file($url$id_produit);
+$page=file('produit.html');
 $liste=array_search($exclure,$page);
 //affichage
 echo '<b>ingrédients exclus</b>';
